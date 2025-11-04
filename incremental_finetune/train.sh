772d1d32b87646be986305a12a81a6d01cd9df04
Geeks_Z <izhw1024@163.com>
2025-11-04 11:16:56 +0800

    增量微调直接用Awesome-CIL中的finetune

--- incremental_finetune/train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=3,4 torchrun --standalone --nproc_per_node=2 main.py --config ./exps/cifar.json
-#python main.py --config=./exps/cifar.json
-#python main.py --config=./exps/cub.json
-#python main.py --config=./exps/inr.json
-#python main.py --config=./exps/ina.json
-#python main.py --config=./exps/omn.json
-#python main.py --config=./exps/vtab.json
--- incremental_finetune/utils/factory.py
+++
@@ -1,8 +0,0 @@
-def get_model(model_name, args):
-    name = model_name.lower()
-    if name == "vit":
-        from models.finetunevit import Learner
-    else:
-        assert 0
-    
-    return Learner(args)
